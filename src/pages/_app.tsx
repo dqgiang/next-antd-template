4d9e5142dadd2f482af43bc6a197b198531fbeaa
Giang Q. Dam <giangq.dam@gmail.com>
2021-05-08 15:19:55 +0700

    fix: Apply hack to disable SSR so that antd style load correctly first time

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,13 @@ import { AppProps } from 'next/app';
 require('../styles/globals.less');
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // HACK: disable SSR by return null to fix (temporarily) issue with antd style not apply correctly at first load
+  // This is intended for dashboard app, using antd, hence this fix is acceptable.
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+    </div>
+  );
 }
 
 export default MyApp;
